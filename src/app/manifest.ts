61b472d0698d8ef9c183a10c8044ca6ebb0c45bd
Soumo-git-hub <deysoumyadyuti@gmail.com>
2026-01-04 19:52:29 +0530

    Added favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,9 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
         theme_color: '#000000',
         icons: [
             {
-                src: '/favicon.ico',
-                sizes: 'any',
-                type: 'image/x-icon',
+                src: '/icon.png',
+                sizes: '512x512',
+                type: 'image/png',
             },
         ],
     }
